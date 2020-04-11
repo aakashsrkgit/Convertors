7815a12acb754c716574ea723178f54516c368da
aakashsrk@gmail.com <aakashsrk@gmail.com>
2020-04-11 13:27:29 -0400

    started text to voice project

---
+++ voice.js
@@ -0,0 +1,7 @@
+// Init Speech Synthesis API
+const synth = window.speechSynthesis;
+
+// DOM Elements
+const textForm = document.querySelector('form');
+const textInput = document.querySelector('#text-input');
+const voiceSelect = document.querySelector('#voice-select');
